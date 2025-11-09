46342fe7a258d12f1a0cbcb050bbecedf56ddb70
vusala <vusalavk-bpa203@code.edu.az>
2025-11-09 22:14:52 +0400

    first committ

---
+++ OneDrive/Desktop/18-ConsoleAppp/Academy System/Repository/Repostories/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repostories.Interfaces
+{
+    public interface IRepository<T>where T : BaseEntity
+    {
+        void Create(T data);
+        void Update(T data);
+        void Delete(T data);
+        T Get(Predicate<T> predicate);
+        List<T> GetAll(Predicate<T>predicate);
+    }
+}
---
+++ OneDrive/Desktop/18-ConsoleAppp/Academy System/Service/Services/Implementations/GroupService.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services.Implementations
+{
+    public class GroupService : IGroupService
+    {
+        private Gr
+        private int _count;
+       
+        public Group Create(Group group)
+        {
+           group.id = _count;
+            _group.Cre
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group GeyById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group Update(int id, Group group)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
